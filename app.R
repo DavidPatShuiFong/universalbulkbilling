18452e985b2969c12050858422cfd8ecf7b69706
David Fong <vkelim@bigpond.com>
2025-03-13 22:07:54 +1100

    Percentage concessional bulk-bill, Set all gap fees

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ library(htmlwidgets)
 
 service_items <- read.csv("./medicarebenefits.csv") |>
   # calculate the proportions
-  # for simplicity, assume the same proportions for bulk-billed patients
+  # for simplicity, assume the same proportions for concessionally bulk-billed patients
   # and privately-billed patients
   mutate(
     service_proportion_raw_bulk = service_proportion_raw,
@@ -30,7 +30,7 @@ service_items <- read.csv("./medicarebenefits.csv") |>
   ) |>
   select(
     fee_names, service_fees, incentive_by_fee = individual_bulkbill_incentive_by_fee,
-    gap_fee, privately_billed,
+    gap_fee,
     service_proportion_bulk, service_proportion_private,
     service_proportion_raw_bulk, service_proportion_raw_private
   )
@@ -63,13 +63,13 @@ individual_bulkbill_incentive <- array(
 universal_bulkbill_incentive <- 0.125
 
 fee_mean <- function(
-  # calculate the current mean fee
+    # calculate the current mean fee
   service_fees,
   fee_names,
   service_proportion_bulk,
   service_proportion_private,
   gap_fee,
-  privately_billed,
+  concessional_bulkbilled,
   individual_bulkbill_incentive,
   individual_bulkbill_incentive_by_fee,
   monash_area
@@ -82,18 +82,18 @@ fee_mean <- function(
   # change row name to the fee names
   rownames(fee_bulkbill_incentive) <- fee_names
 
-  sum(privately_billed * service_fees * service_proportion_private) +
-    sum((1 - privately_billed) * service_fees * service_proportion_bulk) +
+  (1 - concessional_bulkbilled) * sum(service_fees * service_proportion_private) +
+    concessional_bulkbilled * sum(service_fees * service_proportion_bulk) +
     # the calculation above could be simplified, of course!, to
     # sum(service_fees * service_proportion_private), but is kept
     # separate to two different portions to clarify the calculation
 
     # calculate (and add) the individual service bulk-billing incentive applied
-    # to patients currently bulk-billed
-    sum((1 - privately_billed) * service_proportion_private * fee_bulkbill_incentive) +
+    # to patients currently concessionally bulk-billed
+    concessional_bulkbilled * sum(service_proportion_private * fee_bulkbill_incentive) +
 
     # calculate (and add) the private 'gap' fee
-    sum(privately_billed * service_proportion_private * gap_fee)
+    (1 - concessional_bulkbilled) * sum(service_proportion_private * gap_fee)
 }
 
 net_benefit <- function(
@@ -108,7 +108,7 @@ net_benefit <- function(
   service_proportion_bulk,
   service_proportion_private,
   gap_fee,
-  privately_billed,
+  concessional_bulkbilled,
   individual_bulkbill_incentive,
   individual_bulkbill_incentive_by_fee,
   monash_area,
@@ -122,197 +122,238 @@ net_benefit <- function(
 
   # calculate the universal bulk-bill incentive
   # for patients formerly privately-billed
-  sum(privately_billed * service_fees * service_proportion_private) *
+  (1 - concessional_bulkbilled) * sum(service_fees * service_proportion_private) *
     universal_bulkbill_incentive +
-    # calculate (and add) benefit for patients already bulk-billed
-    sum((1 - privately_billed) * service_fees * service_proportion_bulk) *
+    # calculate (and add) benefit for patients already concessionally bulk-billed
+    concessional_bulkbilled *  sum(service_fees * service_proportion_bulk) *
     universal_bulkbill_incentive +
 
     # calculate (and add) the individual service bulk-billing incentive applied
     # to patients formerly privately-billed
-    sum(privately_billed * service_proportion_private * fee_bulkbill_incentive) -
+    (1 - concessional_bulkbilled) * sum(service_proportion_private * fee_bulkbill_incentive) -
 
     # calculate (and subtract) the loss from not charging a private 'gap' fee
-    sum(privately_billed * service_proportion_private * gap_fee)
+    (1 - concessional_bulkbilled) * sum(service_proportion_private * gap_fee)
 }
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  # make horizontal rules more visible
+  tags$head(
+    tags$style(HTML("hr {border-top: 1px solid #000000;}"))
+  ),
 
-    # Application title
-    titlePanel("Universal bulk-billing benefit-loss"),
+  # Application title
+  titlePanel("Universal bulk-billing benefit-loss"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-          selectInput(
-            "monash",
-            "Select Monash area",
-            dimnames(individual_bulkbill_incentive)[[1]]
-            ),
-        ),
-        # Show a plot of the generated distribution
-        mainPanel(
-
-          textOutput("mean_fee"),
-          textOutput("benefit_loss"),
-          textOutput("myoutput"),
-          rHandsontableOutput("mbs_table"),
-
-        )
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      width = 3,
+      selectInput(
+        "monash",
+        "Monash area",
+        dimnames(individual_bulkbill_incentive)[[1]]
+      ),
+      sliderInput(
+        inputId = "concessional_bulkbilled",
+        label = "Concessional bulk-billed (%)",
+        min = 0,
+        max = 100,
+        value = 50
+      ),
+      hr(),
+      sliderInput(
+        inputId = "all_gapfees",
+        label = "Set mean of ALL gap fees ($)",
+        min = 0,
+        max = 100,
+        value = 50
+      ),
+      actionButton(
+        inputId = "all_gapfees_confirm",
+        label = "Confirm"
+      ),
+      hr(),
+      textOutput("mean_fee"),
+      textOutput("benefit_loss")
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      h2("Medicare Benefits"),
+      rHandsontableOutput("mbs_table"),
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    values = reactiveValues(
-      service_table = service_items
+  values = reactiveValues(
+    service_table = service_items
+  )
+
+  # the proportion (0 - 1) of patients who are both bulk-billed
+  # AND currently qualify for the bulk-billing incentives
+  concessional_bulkbilled <- reactive({
+    # change from percentage to (0-1) proportion
+    input$concessional_bulkbilled/100
+  }) |>
+    bindEvent(input$concessional_bulkbilled)
+
+  mean_fee = reactive({
+    v <- trimmed_service_table() |>
+      # if there are empty values in service_proportion_raw_* then replace with 0
+      replace_na(
+        list(
+          service_proportion_raw_bulk = 0,
+          service_proportion_raw_private = 0
+        )
+      ) |>
+      # recalculate service proportions
+      mutate(
+        service_proportion_bulk = service_proportion_raw_bulk/sum(service_proportion_raw_bulk),
+        service_proportion_private = service_proportion_raw_private/sum(service_proportion_raw_private)
+      )
+
+    fee_mean(
+      v$service_fees,
+      v$fee_names,
+      v$service_proportion_bulk,
+      v$service_proportion_private,
+      v$gap_fee,
+      concessional_bulkbilled(),
+      individual_bulkbill_incentive,
+      v$incentive_by_fee,
+      input$monash
+    )
+  }) |>
+    bindEvent(
+      trimmed_service_table(),
+      input$monash,
+      concessional_bulkbilled()
     )
 
-    mean_fee = reactive({
-      browser()
-      v <- values[["service_table"]] |>
-        # if there are empty values in service_proportion_raw_* then replace with 0
-        replace_na(
-          list(
-            service_proportion_raw_bulk = 0,
-            service_proportion_raw_private = 0
-          )
-        ) |>
-        # recalculate service proportions
-        mutate(
-          service_proportion_bulk = service_proportion_raw_bulk/sum(service_proportion_raw_bulk),
-          service_proportion_private = service_proportion_raw_private/sum(service_proportion_raw_private)
+  benefit_loss = reactive({
+    v <- values[["service_table"]] |>
+      # get rid of any rows with empty values
+      filter(!is.na(fee_names) & !is.na(service_fees) &
+               !is.na(incentive_by_fee) & !is.na(gap_fee) &
+               !is.na(service_proportion_bulk) &
+               !is.na(service_proportion_private))
+    net_benefit(
+      v$service_fees,
+      v$fee_names,
+      v$service_proportion_bulk,
+      v$service_proportion_private,
+      v$gap_fee,
+      concessional_bulkbilled(),
+      individual_bulkbill_incentive,
+      v$incentive_by_fee,
+      input$monash,
+      universal_bulkbill_incentive
+    )
+  }) |>
+    bindEvent(
+      trimmed_service_table(),
+      input$monash,
+      concessional_bulkbilled()
+    )
+
+  adjust_proportions = reactive({
+    browser()
+    v <- hot_to_r(input$mbs_table) |>
+      # if there are empty values in service_proportion_raw_* then replace with 0
+      replace_na(
+        list(
+          service_proportion_raw_bulk = 0,
+          service_proportion_raw_private = 0
         )
-      values[["service_table"]] = v
-
-      v <- v |>
-        # get rid of any rows with empty values
-        filter(!is.na(fee_names) & !is.na(service_fees) &
-                 !is.na(incentive_by_fee) & !is.na(gap_fee) &
-                 !is.na(privately_billed) & !is.na(service_proportion_bulk) &
-                 !is.na(service_proportion_private))
-
-      fee_mean(
-        v$service_fees,
-        v$fee_names,
-        v$service_proportion_bulk,
-        v$service_proportion_private,
-        v$gap_fee,
-        v$privately_billed,
-        individual_bulkbill_incentive,
-        v$incentive_by_fee,
-        input$monash
+      ) |>
+      # recalculate service proportions
+      mutate(
+        service_proportion_bulk = service_proportion_raw_bulk/sum(service_proportion_raw_bulk),
+        service_proportion_private = service_proportion_raw_private/sum(service_proportion_raw_private)
       )
-    })
+    values[["service_table"]] = v
+  }) |>
+    bindEvent(input$mbs_table)
 
-    benefit_loss = reactive({
-      v <- values[["service_table"]] |>
-        # get rid of any rows with empty values
-        filter(!is.na(fee_names) & !is.na(service_fees) &
-                 !is.na(incentive_by_fee) & !is.na(gap_fee) &
-                 !is.na(privately_billed) & !is.na(service_proportion_bulk) &
-                 !is.na(service_proportion_private))
-      net_benefit(
-        v$service_fees,
-        v$fee_names,
-        v$service_proportion_bulk,
-        v$service_proportion_private,
-        v$gap_fee,
-        v$privately_billed,
-        individual_bulkbill_incentive,
-        v$incentive_by_fee,
-        input$monash,
-        universal_bulkbill_incentive
-      )
-    })
+  output$mean_fee <- renderText({
+    sprintf(
+      "Mean fee: $%.2f",
+      mean_fee()
+    )
+  })
+  output$benefit_loss <- renderText({
+    sprintf(
+      "Net benefit: $%.2f",
+      benefit_loss()
+    )
+  })
 
-    adjust_proportions = reactive({
-      browser()
-      v <- values[["service_table"]]
-      v <- values[["service_table"]] |>
+  trimmed_service_table <- reactive({
+    values[["service_table"]] |>
+      # get rid of any rows with empty values
+      filter(!is.na(fee_names) & !is.na(service_fees) &
+               !is.na(incentive_by_fee) & !is.na(gap_fee) &
+               !is.na(service_proportion_bulk) &
+               !is.na(service_proportion_private))
+
+  }) |>
+    bindEvent(values[["service_table"]])
+
+  output$mbs_table = renderRHandsontable({
+    DT = NULL
+    if (!is.null(input$mbs_table)) {
+      DT = hot_to_r(input$mbs_table) |>
         # if there are empty values in service_proportion_raw_* then replace with 0
-        replace_na(
-          list(
-            service_proportion_raw_bulk = 0,
-            service_proportion_raw_private = 0
-          )
-        ) |>
-        # recalculate service proportions
-        mutate(
-          service_proportion_bulk = service_proportion_raw_bulk/sum(service_proportion_raw_bulk),
-          service_proportion_private = service_proportion_raw_private/sum(service_proportion_raw_private)
-        )
-      values[["service_table"]] = v
-    })
-
-    output$distPlot <- renderPlot({
-      x <- faithful[, 2]  # Old Faithful Geyser data
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      hist(x, breaks = bins, col = 'darkgray', border = 'white',
-           xlab = 'Waiting time to next eruption (in mins)',
-           main = 'Histogram of waiting times')
-    })
-
-    output$myoutput <- renderText({values[["service_table"]]$fee_names})
-
-    output$mean_fee <- renderText({
-      sprintf(
-        "Mean fee: $%.2f",
-        mean_fee()
-      )
-    })
-    output$benefit_loss <- renderText({
-      sprintf(
-        "Net benefit: $%.2f",
-        benefit_loss()
-      )
-    })
-
-    output$mbs_table = renderRHandsontable({
-      DT = NULL
-      if (!is.null(input$mbs_table)) {
-        DT = hot_to_r(input$mbs_table)
         # if a new row is created, it may contain lots of invalid NA values
-        values[["service_table"]] = DT
-      } else if (!is.null(values[["service_table"]])) {
-        DT = values[["service_table"]]
-      }
-
-      if (!is.null(DT))
-        rhandsontable(
-          DT
+        replace_na(
+          list(
+            service_proportion_raw_bulk = 0,
+            service_proportion_raw_private = 0
+          )
         ) |>
-        hot_context_menu(
-          # cannot change columns
-          # (but rows can be added/removed)
-          allowColEdit = FALSE
-        ) |>
-        hot_col(
-          col = "incentive_by_fee",
-          # only allow two choices
-          type = "dropdown", source = c("single", "triple")
-        ) |>
-        hot_col(
-          col = c("service_proportion_bulk", "service_proportion_private"),
-          format = "0.00%",
-          readOnly = TRUE,
-        ) |>
-        hot_validate_numeric(
-          col = c(
-            "service_fees", "privately_billed",
-            "service_proportion_raw_bulk", "service_proportion_raw_private"
-          ),
-          # cannot be negative numbers
-          min = 0
-        ) |>
-        hot_validate_numeric(
-          col = c("privately_billed"),
-          min = 0, max = 1
+        # recalculate service proportions
+        mutate(
+          service_proportion_bulk = service_proportion_raw_bulk/sum(service_proportion_raw_bulk),
+          service_proportion_private = service_proportion_raw_private/sum(service_proportion_raw_private)
         )
-    })
+      values[["service_table"]] = DT
+    } else if (!is.null(values[["service_table"]])) {
+      DT = values[["service_table"]]
+    }
+
+    if (!is.null(DT))
+      rhandsontable(
+        DT
+      ) |>
+      hot_context_menu(
+        # cannot change columns
+        # (but rows can be added/removed)
+        allowColEdit = FALSE
+      ) |>
+      hot_col(
+        col = "incentive_by_fee",
+        # only allow two choices
+        type = "dropdown", source = c("single", "triple")
+      ) |>
+      hot_col(
+        col = c("service_proportion_bulk", "service_proportion_private"),
+        format = "0.00%",
+        readOnly = TRUE,
+      ) |>
+      hot_validate_numeric(
+        col = c(
+          "service_fees",
+          "service_proportion_raw_bulk", "service_proportion_raw_private"
+        ),
+        # cannot be negative numbers
+        min = 0
+      )
+  })
 
 }
 
